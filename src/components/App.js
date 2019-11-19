2c62b416a13fa6774ae2dc5a97a8380b1dbddf84
Freya Gehrke <freya.gehrke@xing.com>
2019-11-19 16:56:06 +0100

    Add Header and basic styling

--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,20 @@ import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Route path="/" exact component={StreamList} />
-      <Route path="/streams/new" component={StreamCreate} />
-      <Route path="/streams/edit" component={StreamEdit} />
-      <Route path="/streams/delete" component={StreamDelete} />
-      <Route path="/streams/show" component={StreamShow} />
-    </BrowserRouter>
+    <div className="ui container">
+      <BrowserRouter>
+        <Header />
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" component={StreamCreate} />
+        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/delete" component={StreamDelete} />
+        <Route path="/streams/show" component={StreamShow} />
+      </BrowserRouter>
+    </div>
   );
 };
 
