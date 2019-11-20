e22501c2dd7697fda8d46272a9419cbce3a463fb
Freya Gehrke <freya.gehrke@xing.com>
2019-11-20 18:37:15 +0100

    Implement submit function with button

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -13,15 +13,22 @@ class StreamCreate extends React.Component {
     );
   }
 
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
   render() {
     return (
-      <form className="ui form">
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form">
         <Field name="title" label="Enter title" component={this.renderInput} />
         <Field
           name="description"
           label="Enter description"
           component={this.renderInput}
         />
+        <button className="ui button primary">Create</button>
       </form>
     );
   }
